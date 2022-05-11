1076b2d9c263556803be331c7166986483dea20b
dingff <@521125z>
2022-05-11 17:09:44 +0800

    feat:增加brush

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ $ npm install brush-log --save
 ## Usuage
 ```javascript
 import log from 'brush-log';
+
+// 简单用法
 log('列表', { a: 1 });
 
 // 你可以快速自定义当前打印的背景色
@@ -20,4 +22,10 @@ window.brushLog = {
   backgroundColor: 'pink',
   border: '2px dashed #000',
 }
+
+// 或许你发现上面的写法导致控制台打印的代码行数不太准，试试下面这种写法
+import { brush } from 'brush-log';
+
+console.log(...brush('列表', { a: 1 }, 'red'));
+
 ```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const getStyleName = (name) => {
   return name.replace(/([A-Z])/g, "-$1").toLowerCase()
 }
-const log = (title, data, bg) => {
+const mixStyles = (bg) => {
   const globalStyles = Object.entries(window.brushLog || {}).reduce((acc,curr) => {
     return `${acc}${getStyleName(curr[0])}:${curr[1]};`
   }, '')
@@ -9,19 +9,25 @@ const log = (title, data, bg) => {
     color: #000;
     margin: 2px 0 4px 0;
     border-radius: 14px;
-    padding: 4px 10px;
+    padding: 6px 10px;
     font-weight: bold;
-    background: skyblue;
-    border: 2px dashed #000;
+    background: #CDE5F9;
+    border: 1px dashed #000;
     ${globalStyles}
     ${bg ? `background: ${bg};` : ''}
     `
+  return styles
+} 
+const log = (title, data, bg) => {
   if (Object.prototype.toString.call(data) === '[object Object]') {
-    console.group(`%c${title}`, styles, data)
+    console.groupCollapsed(`%c${title}`, mixStyles(bg), data)
     console.table(data)
     console.groupEnd()
   } else {
-    console.log(`%c${title}`, styles, data)
+    console.log(`%c${title}`, mixStyles(bg), data)
   }
 }
 export default log
+export const brush = (title, data, bg) => {
+  return [`%c${title}`, mixStyles(bg), data]
+}
